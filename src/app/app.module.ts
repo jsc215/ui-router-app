66d2dd162c7ce79a61a17f392caeaf66c4162883
jsc215 <jeffcomertennis@gmail.com>
2019-03-08 22:04:34 -0500

    initial and add ui-router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
+import { APP_STATES } from './app.states';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { UIRouterModule, UIView } from '@uirouter/angular';
+import { routerConfigFn } from './router.config';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UIRouterModule.forRoot({
+      states: APP_STATES,
+      useHash: true,
+      config: routerConfigFn
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/app.states.ts
@@ -0,0 +1,12 @@
+import { AppComponent } from './app.component';
+
+export const appState = {
+  name: 'app',
+  url: '',
+  // redirectTo: 'welcome',
+  component: AppComponent,
+};
+
+export const APP_STATES = [
+  appState
+];
